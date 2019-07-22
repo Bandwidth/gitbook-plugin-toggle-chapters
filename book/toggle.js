fd6a421ad7bbc648981d49a8875bbe3d57f05518
John Chavez <jchavez@bandwidth.com>
2019-07-22 16:16:48 -0400

    javascript changes to get rid of the eyes in the table of contents

--- book/toggle.js
+++ book/toggle.js
@@ -15,26 +15,43 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
     $children = $chapter.children('ul.articles');
     $parent = $chapter.parent();
     $siblings = $chapter.siblings().children('ul.articles');
-    $('li.chapter').children('ul.articles').hide();
-    $('li.chapter').hover(function(){
-      $(this).children('.expand').css('visibility','visible')
-    }, function(){
-      $(this).children('.expand').css('visibility','');
-    });
+	$('li.chapter').children('ul.articles').hide();
+	
+	$('.expand').removeClass('fa-eye');
+	$('.expand').removeClass('fa-eye-slash');
+
+	//The below code can be used to display chevrons aligned to the right.  
+	//Just remove the ```$('.expand').removeClass('fa-eye-slash');``` above
+	/**
+	$('.expand').css({'visibility': 'visible', 'float':'right'});
+	$('.expand').addClass('fa-angle-up');
     $('.expand').click(function(){
-      $(this).toggleClass('fa-eye-slash');
-      if ($(this).hasClass('fa-eye-slash')){
-        $(this).next('.articles').show();
+      if ($(this).hasClass('fa-angle-up')){
+		$(this).next('.articles').show();
+		$(this).removeClass('fa-angle-up');
+		$(this).addClass('fa-angle-down');
+		$('.fa-angle-down').css('color', '#00bef0');
       } else {
-        $(this).next('.articles').hide();
-      };
-    });
+		$(this).next('.articles').hide();
+		$(this).removeClass('fa-angle-down');
+		$(this).addClass('fa-angle-up');
+		$('.fa-angle-up').css('color', '');
+	  };
+	});
+	$('.fa-eye-slash.fa-angle-up').removeClass('fa-angle-up');
+	$('.fa-eye-slash').addClass('fa-angle-down');
+	$('.fa-eye-slash').removeClass('fa-eye-slash');
+	$('.fa-angle-down').css('color', '#00bef0');
+	 */
+
+
     expand($chapter);
-    $('li.chapter.active').children('.expand').addClass('fa-eye-slash');
+    $('li.chapter.active').children('.expand').addClass('fa-angle-down');
 
     if ($children.length > 0) {
       $children.show();
-    }
+	}
+
   });
 
 });
